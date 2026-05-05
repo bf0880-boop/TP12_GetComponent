d6463f35b8cd78856f573c5e551e899f69ce09bb
bf0880-boop <bf0880@est.ort.edu.ar>
2026-05-05 14:27:08 -0300

    Initial commit

---
+++ Assets/Scripts/Colision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colision : MonoBehaviour
+{
+
+    CubeScript cubeData;
+
+    void OnCollisionEnter (Collision col){
+        Debug.Log ("Hubo colisión");
+        cubeData = col.gameObject.GetComponent<CubeScript>();
+        Debug.Log ("Nombre del otro objeto: " + cubeData.objectName);
+    }
+}
---
+++ Assets/Scripts/CubeScript.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+public string objectName;
+public int value;
+}
